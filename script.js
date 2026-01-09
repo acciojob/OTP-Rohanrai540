f799a084b01e1e2c5be7dc4c2cd64c1c76850be1
Rohan Rai <92577040+Rohanrai540@users.noreply.github.com>
2026-01-09 19:59:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 //your JS code here. If required.
 const codes = document.querySelectorAll('.code');
 
+// focus first input on load
 codes[0].focus();
 
 codes.forEach((code, index) => {
 
-    code.addEventListener('input', (e) => {
-        if (e.target.value && index < codes.length - 1) {
-            codes[index + 1].focus();
-        }
-    });
+  code.addEventListener('input', () => {
+    if (code.value && index < codes.length - 1) {
+      codes[index + 1].focus();
+    }
+  });
 
-    code.addEventListener('keydown', (e) => {
-        if (e.key === 'Backspace') {
-            if (code.value === '' && index > 0) {
-                codes[index - 1].focus();
-                codes[index - 1].value = '';
-            }
-        }
-    });
+  code.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {
+      if (code.value === '' && index > 0) {
+        codes[index - 1].focus();
+      }
+    }
+  });
 
 });
+
 
